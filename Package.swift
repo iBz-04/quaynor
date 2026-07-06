9fa171427e0b3a77ea0ad8fe0675c600e220a317
Ibrahim Rayamah <issakaibrahimrayamah@gmail.com>
2026-07-06 15:36:17 +0300

    Point Swift binary target at quaynor-swift-0.1.1

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CQuaynorFFI",
-            url: "https://github.com/iBz-04/quaynor/releases/download/quaynor-swift-0.1.0/QuaynorFFI.xcframework.zip",
-            checksum: "2da0e469a64f5dcfb98c80ce6f98c602f9ca26828e64d877a9c763ca709b7197"
+            url: "https://github.com/iBz-04/quaynor/releases/download/quaynor-swift-0.1.1/QuaynorFFI.xcframework.zip",
+            checksum: "0b70cc398cefac513e42ec96a3cb1083ad5112a4ddf1d078779f875e56c5d6d4"
         ),
         .target(
             name: "QuaynorFFI",
